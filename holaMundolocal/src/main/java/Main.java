3c514224662845c7fe37edd5d34fdaef296ee4d7
davis <davis@localhost>
2022-04-11 17:35:26 +0200

    commit hola mundo restificado

--- holaMundolocal/src/main/java/Main.java
+++ holaMundolocal/src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola mundo.");
     }
     
 }
